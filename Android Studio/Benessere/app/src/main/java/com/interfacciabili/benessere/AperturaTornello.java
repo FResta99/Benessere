19cb02ad1b3cb52ca3b115d6d3fbb42627deeb4a
Davide <48534020+davidepalladino@users.noreply.github.com>
2020-12-28 13:49:36 +0100

    Sviluppato l'IF-29 e riorganizzato il repository

---
+++ Android Studio/Benessere/app/src/main/java/com/interfacciabili/benessere/AperturaTornello.java
@@ -0,0 +1,49 @@
+package com.interfacciabili.benessere;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class AperturaTornello extends AppCompatActivity implements View.OnClickListener {
+    private static Button buttonTurnstile;
+
+    private static final String serverIP = "192.168.0.16";
+    private static final int serverPort = 125;
+    private Socket socket;
+    private Thread thread;
+    PrintWriter output;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        buttonTurnstile = (Button) findViewById(R.id.buttonLed);
+        buttonTurnstile.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    if (socket == null) {
+                        socket = new Socket(serverIP, serverPort);
+                        output = new PrintWriter(socket.getOutputStream(), true);
+                    }
+                    output.println((int) 1);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        thread.start();
+    }
+}
---
+++ PlatformIO/Benessere/src/main.cpp
@@ -0,0 +1,72 @@
+#include <Arduino.h>
+
+#include <ESP8266WiFi.h>
+#include <Servo.h>
+
+#include "configMain.h"
+#include "../configWiFi.h"
+#include "../configServer.h"
+
+void checkTimeoutTurnstile();
+
+WiFiServer server(portServer);
+
+Servo turnstile;
+unsigned long timeoutTurnstile = 0;
+
+void setup() {
+  Serial.begin(115200);
+
+  Serial.print("\nConnection to WiFi..");
+  WiFi.begin(wifiSSID, wifiPassword);
+  WiFi.hostname(wifiHostname);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.print("\n\tIP address: ");
+  Serial.println(WiFi.localIP());
+  Serial.print("\tHostname: ");
+  Serial.println(WiFi.hostname());
+
+  server.begin();
+
+  turnstile.attach(pinServo);
+  turnstile.write(valueClose);
+}
+
+void loop() {
+  WiFiClient client = server.available();
+  if (client) {
+    while (client.connected()) {
+      int action = 0;
+
+      while (client.available() != 0) {
+        action = client.read();
+        break;
+      }
+      delay(10);
+
+      switch (action) {
+          case '1':
+            turnstile.write(valueOpen);
+            timeoutTurnstile = millis() + timeOpenedTurnstile;
+            action = 0;
+            
+            break;
+      }
+
+      checkTimeoutTurnstile();
+    }
+    client.stop();
+
+    checkTimeoutTurnstile();
+  }
+}
+
+void checkTimeoutTurnstile() {
+  if ((millis() > timeoutTurnstile) && (timeoutTurnstile != 0)) {
+    timeoutTurnstile = 0;
+    turnstile.write(valueClose);
+  }
+}
